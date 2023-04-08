3f246c80879ec8bf1f3259009167cc3324eb1282
VukkertAS <VukkertAS@411D-9>
2023-04-08 10:30:47 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Times;
+
+Time time2 = new Time(1, 59, 59);
+Time time = new Time(1,59,60);
+Time time3 = time2 + time;
+Console.WriteLine(time2.ToString());
+Console.WriteLine(time.ToString());
+Console.WriteLine(time3.ToString());
---
+++ Time.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Times
+{
+    class Time
+    {
+        public Time(int hours = 0, int minutes = 0, int seconds = 0)
+        {
+            Hours = hours;
+            Minutes = minutes;
+            Seconds = seconds;
+        }
+       
+        public static bool operator >(Time time1, Time time2)
+        {
+            return time1.ToSeconds() > time2.ToSeconds();
+        }
+        public static bool operator <(Time time1, Time time2)
+        {
+            return time1.ToSeconds() < time2.ToSeconds();
+        }
+        public static bool operator ==(Time time1, Time time2)
+        {
+            return time1.ToSeconds() == time2.ToSeconds();
+        }
+        public static bool operator !=(Time time1, Time time2)
+        {
+            return time1.ToSeconds() != time2.ToSeconds();
+        }
+        public static Time operator +(Time time1, Time time2)
+        {
+            return new Time { Seconds = time1.Seconds + time2.Seconds, Minutes = time1.Minutes + time2.Minutes, Hours = time1.Hours + time2.Hours };
+        }
+        public static Time operator -(Time time1, Time time2)
+        {
+            return new Time { Seconds = time1.Seconds - time2.Seconds, Minutes = time1.Minutes - time2.Minutes, Hours = time1.Hours - time2.Hours };
+        }
+        public static Time operator --(Time time)
+        {
+            return new Time { Seconds = time.Seconds - 1 };
+        }
+        public static Time operator ++(Time time)
+        {
+            return new Time { Seconds = time.Seconds + 1, Minutes = time.Minutes, Hours = time.Hours };
+        }
+        int ToSeconds()
+        {
+            return Seconds + Minutes * 60 + Hours * 3600;
+        }
+        private int minutes;
+        public int Minutes
+        {
+            set
+            {
+                if (value < 0 || value >= 60)
+                {
+                    while(value >= 60)
+                    {
+                        Hours++;
+                        value -= 60;
+                    }
+                    if(value >= 0)
+                    {
+                        minutes = value;
+                    }
+                }
+                else
+                {
+                    minutes = value;
+                }
+            }
+            get { return minutes; }
+        }
+        private int hours;
+        public int Hours
+        {
+            set
+            {
+                hours = value;
+            }
+            get { return hours; }
+        }
+        private int seconds;
+        public int Seconds
+        {
+            set
+            {
+                if (value < 0 || value >= 60)
+                {
+                    while (value >= 60)
+                    {
+                        Minutes++;
+                        value -= 60;
+                    }
+                    if (value >= 0)
+                    {
+                        seconds = value;
+                    }
+                }
+                else
+                {
+                    seconds = value;
+                }
+            }
+            get { return seconds; }
+        }
+        public override string ToString()
+        {
+            return $"Hours: {hours}, Minutes: {minutes}, Seconds: {seconds}.";
+        }
+    }
+}
